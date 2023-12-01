7aac987a0d515e3ca56398c7a51442c14270efca
Jesse Zhou <69114052+JesseZhou-1@users.noreply.github.com>
2023-12-01 17:39:47 -0600

    Update sum_zhou_bootstrap.py

--- zhou_2019/bootstrap/sum_zhou_bootstrap.py
+++ zhou_2019/bootstrap/sum_zhou_bootstrap.py
@@ -2,7 +2,7 @@ import os
 import pandas as pd
 
 # Define the path where the files are located
-base_path = '/Users/changhuizhou/Desktop/Midway_MCE_results'
+base_path = '/Users/jessezhou/Desktop/Midway_MCE_results'
 folder = 'zhou_2019'
 path = os.path.join(base_path, folder, '')
 
