2a7af483ece33dbfb79c3b0d952a7c3590af348a
Jesse Zhou <69114052+JesseZhou-1@users.noreply.github.com>
2023-12-01 17:38:47 -0600

    Update and rename plot_MCE_1.py to plot_MCE_2.py

--- MCEs/MCE_2/plot_MCE_2.py
+++ MCEs/MCE_2/plot_MCE_2.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 plt.rcParams["font.family"] = "Times New Roman"
 
 # Reading datasets
-path = '/Users/changhuizhou/Desktop/Midway_MCE_results/NONLIN/'
+path = '/Users/jessezhou/Desktop/Midway_MCE_results/_MCE_2/'
 
 data_2k = pd.read_csv(path + "2k/2k_summary_statistics.csv", index_col=0)
 data_4k = pd.read_csv(path + "4k/4k_summary_statistics.csv", index_col=0)
