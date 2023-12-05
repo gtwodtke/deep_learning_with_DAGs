a4bc413fedcb151ffcdc1fbdd654532bf4392545
Jesse Zhou <69114052+JesseZhou-1@users.noreply.github.com>
2023-12-05 13:57:59 -0600

    Update plot_hyperparameters_1.py

--- MCEs/Hyperparameters_1/plot_hyperparameters_1.py
+++ MCEs/Hyperparameters_1/plot_hyperparameters_1.py
@@ -44,7 +44,7 @@ def create_bar_plot(column, title, datasets, labels):
     plt.tight_layout()
     plt.savefig(path + f"Hyperparameter_{title}.png", dpi=300)
 
-path = '/Users/jessezhou/Desktop/Midway_MCE_results/Hyperparameter/'
+path = '/Users/jessezhou/Desktop/Hyperparameters_1/'
 
 
 # Load all datasets
