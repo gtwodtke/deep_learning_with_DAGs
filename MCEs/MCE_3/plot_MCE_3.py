07b836da65062f9bb9cbd896479ff467c555d8f5
Jesse Zhou <69114052+JesseZhou-1@users.noreply.github.com>
2023-12-07 11:26:49 -0600

    Add files via upload

---
+++ MCEs/MCE_3/plot_MCE_3.py
@@ -0,0 +1,138 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+plt.rcParams["font.family"] = "Times New Roman"
+
+# Reading datasets
+path = '/Users/jessezhou/Desktop/BIN/'
+
+data_2k = pd.read_csv(path + "2k/2k_summary_statistics.csv", index_col=0)
+data_4k = pd.read_csv(path + "4k/4k_summary_statistics.csv", index_col=0)
+data_8k = pd.read_csv(path + "8k/8k_summary_statistics.csv", index_col=0)
+data_16k = pd.read_csv(path + "16k/16k_summary_statistics.csv", index_col=0)
+data_32k = pd.read_csv(path + "32k/32k_summary_statistics.csv", index_col=0)
+data_64k = pd.read_csv(path + "64k/64k_summary_statistics.csv", index_col=0)
+data_128k = pd.read_csv(path + "128k/128k_summary_statistics.csv", index_col=0)
+
+# Specify the effects of interest
+desired_effects = [
+    "cGNF_ATE (A->Y)",
+    "cGNF_PSE (A->Y)",
+    "cGNF_PSE (A->L->Y)",
+    "cGNF_PSE (A->M->Y)",
+    "cGNF_ATE (A->M)",
+    "cGNF_NDE",
+    "cGNF_NIE"
+]
+
+# Extract the data for the desired effects
+sample_sizes = [2000, 4000, 8000, 16000, 32000, 64000, 128000]
+
+bias_values_cgnf = {
+    effect: [
+        data_2k.loc[effect, 'Bias'],
+        data_4k.loc[effect, 'Bias'],
+        data_8k.loc[effect, 'Bias'],
+        data_16k.loc[effect, 'Bias'],
+        data_32k.loc[effect, 'Bias'],
+        data_64k.loc[effect, 'Bias'],
+        data_128k.loc[effect, 'Bias']
+    ] for effect in desired_effects
+}
+
+sd_values_cgnf = {
+    effect: [
+        data_2k.loc[effect, 'SD'],
+        data_4k.loc[effect, 'SD'],
+        data_8k.loc[effect, 'SD'],
+        data_16k.loc[effect, 'SD'],
+        data_32k.loc[effect, 'SD'],
+        data_64k.loc[effect, 'SD'],
+        data_128k.loc[effect, 'SD']
+    ] for effect in desired_effects
+}
+
+rootmse_values_cgnf = {
+    effect: [
+        data_2k.loc[effect, 'Root_MSE'],
+        data_4k.loc[effect, 'Root_MSE'],
+        data_8k.loc[effect, 'Root_MSE'],
+        data_16k.loc[effect, 'Root_MSE'],
+        data_32k.loc[effect, 'Root_MSE'],
+        data_64k.loc[effect, 'Root_MSE'],
+        data_128k.loc[effect, 'Root_MSE']
+    ] for effect in desired_effects
+}
+
+label_effect = [f"ATE$_{{A \\rightarrow Y}}$",
+                f"PSE$_{{A \\rightarrow Y}}$",
+                f"PSE$_{{A \\rightarrow L \\rightsquigarrow Y}}$",
+                f"PSE$_{{A \\rightarrow M \\rightarrow Y}}$",
+                f"ATE$_{{A \\rightarrow M}}$",
+                f"NDE$_{{A \\rightarrow M}}$",
+                f"NIE$_{{A \\rightarrow L \\rightarrow M}}$"]
+
+# Adjusting the styles to make them unique for each effect
+
+# Define unique styles for the lines
+line_styles_unique = ['-', '--', '-.', ':', (0, (3, 5, 1, 5)), (0, (3, 1, 1, 1)), (0, (5, 10))]
+marker_unique = ['o', 's', '^', '+', 'x', 'd', 'p']  # circle, square, triangle_up, triangle_down, triangle_left, triangle_right, pentagon
+colors_unique = ['black', 'grey', 'black', 'grey', 'black', 'grey', 'black']
+
+# Plotting Bias with unique styles
+plt.figure(figsize=(10, 7))
+for idx, effect in enumerate(desired_effects):
+    plt.plot(sample_sizes, bias_values_cgnf[effect], label=label_effect[idx], linestyle=line_styles_unique[idx], color=colors_unique[idx], marker=marker_unique[idx])
+# plt.title("Bias for Specified cGNF Effects")
+plt.xlabel("Sample Size")
+plt.ylabel("Bias")
+plt.grid(True, which='both', linestyle='--', linewidth=0.5)
+plt.legend()
+plt.tight_layout()
+plt.savefig(path + "Bias_for_Specified_cGNF_Effects.png", dpi=300)
+plt.show()
+
+# Plotting SD with unique styles
+plt.figure(figsize=(10, 7))
+for idx, effect in enumerate(desired_effects):
+    plt.plot(sample_sizes, sd_values_cgnf[effect], label=label_effect[idx], linestyle=line_styles_unique[idx], color=colors_unique[idx], marker=marker_unique[idx], markersize=6)
+# plt.title("Standard Deviation for Specified cGNF Effects")
+plt.xlabel("Sample Size")
+plt.ylabel("Standard Deviation")
+plt.grid(True, which='both', linestyle='--', linewidth=0.5)
+plt.legend()
+plt.ylim(bottom=0)  # Set the lower limit of y-axis to 0
+plt.tight_layout()
+plt.savefig(path + "SD_for_Specified_cGNF_Effects.png", dpi=300)
+plt.show()
+
+# Plotting Root_MSE with unique styles
+plt.figure(figsize=(10, 7))
+for idx, effect in enumerate(desired_effects):
+    plt.plot(sample_sizes, rootmse_values_cgnf[effect], label=label_effect[idx], linestyle=line_styles_unique[idx], color=colors_unique[idx], marker=marker_unique[idx])
+# plt.title("Root_MSE for Specified cGNF Effects")
+plt.xlabel("Sample Size")
+plt.ylabel("RMSE")
+plt.grid(True, which='both', linestyle='--', linewidth=0.5)
+plt.legend()
+plt.ylim(bottom=0)  # Set the lower limit of y-axis to 0
+plt.tight_layout()
+plt.savefig(path + "Root_MSE_for_Specified_cGNF_Effects.png", dpi=300)
+plt.show()
+
+
+bias_values_cgnf_tab = pd.DataFrame(bias_values_cgnf, index=['2k', '4k', '8k', '16k', '32k', '64k', '128k']).T.round(3)
+sd_values_cgnf_tab = pd.DataFrame(sd_values_cgnf, index=['2k', '4k', '8k', '16k', '32k', '64k', '128k']).T.round(3)
+rootmse_values_cgnf_tab = pd.DataFrame(rootmse_values_cgnf, index=['2k', '4k', '8k', '16k', '32k', '64k', '128k']).T.round(3)
+
+
+# Combining all four tables: Bias, SD, Root_MSE, and Ratio Root_MSE
+combined_df = pd.concat([
+    bias_values_cgnf_tab.add_prefix("Bias_"),
+    sd_values_cgnf_tab.add_prefix("SD_"),
+    rootmse_values_cgnf_tab.add_prefix("RootMSE_"),
+], axis=1)
+
+# Saving the combined table to an Excel file
+combined_df.to_excel(path + "combined_data.xlsx")
---
+++ MCEs/MCE_3/sum_MCE_3.py
@@ -0,0 +1,82 @@
+import os
+import pandas as pd
+
+# Define the path where the files are located
+base_path = '/Users/jessezhou/Desktop/BIN'
+folder = '128k'
+path = os.path.join(base_path, folder, '')
+
+# List of file names to be appended
+file_names = [f'128k_final_results_{i}.csv' for i in range(1, 11)]
+
+# Initialize an empty DataFrame to store the combined results
+final_results_df = pd.DataFrame()
+
+# Iterate through the file names and read each CSV file
+for file_name in file_names:
+    file_path = os.path.join(path, file_name)
+    temp_df = pd.read_csv(file_path)
+    final_results_df = pd.concat([final_results_df, temp_df], ignore_index=True)
+
+# Save the combined results to a new CSV file
+final_results_file_path = os.path.join(path, '128k_final_results.csv')
+final_results_df.to_csv(final_results_file_path, index=False)
+
+final_results_df = pd.read_csv(final_results_file_path)
+
+
+#cGNF
+
+final_results_df['cGNF_ATE (A->Y)'] = final_results_df['E_Y_A_1_sim'] - final_results_df['E_Y_A_0_sim']
+final_results_df['cGNF_PSE (A->Y)'] = final_results_df['E_Y_A_1_L_0_M_0_sim'] - final_results_df['E_Y_A_0_sim']
+final_results_df['cGNF_PSE (A->L->Y)'] = final_results_df['E_Y_A_1_sim'] - final_results_df['E_Y_A_1_L_0_sim']
+final_results_df['cGNF_PSE (A->M->Y)'] = final_results_df['E_Y_A_1_L_0_sim'] - final_results_df['E_Y_A_1_L_0_M_0_sim']
+final_results_df['cGNF_ATE (A->M)'] = final_results_df['E_M_A_1_sim'] - final_results_df['E_M_A_0_sim']
+final_results_df['cGNF_NDE'] = final_results_df['E_M_A_1_L_0_sim'] - final_results_df['E_M_A_0_sim']
+final_results_df['cGNF_NIE'] = final_results_df['E_M_A_1_sim'] - final_results_df['E_M_A_1_L_0_sim']
+
+# Save the DataFrame with new columns to a new CSV file or overwrite the previous file
+final_results_df.to_csv(final_results_file_path, index=False)
+
+# You can now use final_results_df for the subsequent analysis
+results_df = final_results_df
+
+# Define the true values
+true_values = {
+    'cGNF_ATE (A->Y)': 0.052607262,
+    'cGNF_PSE (A->Y)': 0.043036681,
+    'cGNF_PSE (A->L->Y)': -0.000499979,
+    'cGNF_PSE (A->M->Y)': 0.01007056,
+    'cGNF_ATE (A->M)': 0.130511726,
+    'cGNF_NDE': 0.13115862,
+    'cGNF_NIE': -0.000646894
+}
+
+
+# Compute the mean, variance, and MSE for the specified columns
+columns_to_analyze = ['cGNF_ATE (A->Y)', 'cGNF_PSE (A->Y)', 'cGNF_PSE (A->L->Y)', 'cGNF_PSE (A->M->Y)', 'cGNF_ATE (A->M)',
+                      'cGNF_NDE', 'cGNF_NIE']
+summary_stats = {}
+
+for col in columns_to_analyze:
+    mean_value = results_df[col].mean()
+    bias_value = mean_value - true_values[col]
+    variance_value = results_df[col].var()
+    mse_value = ((results_df[col] - true_values[col]) ** 2).mean()
+    sd_value = variance_value ** (1/2)
+    root_mse_value = mse_value ** (1/2)
+
+    summary_stats[col] = {
+        'Bias': bias_value,
+        'SD': sd_value,
+        'Root_MSE': root_mse_value,
+    }
+
+
+# Convert the summary statistics to a DataFrame and print/save if needed
+summary_stats_df = pd.DataFrame.from_dict(summary_stats, orient='index')
+print(summary_stats_df)
+
+# Optionally, save the summary statistics to a CSV file
+summary_stats_df.to_csv(path + '128k_summary_statistics.csv', index=True)
+
