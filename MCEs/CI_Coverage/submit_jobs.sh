fbe8cab342004fcbfe45e53c08f83d53a39cfc47
Jesse Zhou <69114052+JesseZhou-1@users.noreply.github.com>
2023-12-18 09:54:18 -0600

    Update submit_jobs.sh

--- MCEs/CI_Coverage/submit_jobs.sh
+++ MCEs/CI_Coverage/submit_jobs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for n in {70..99}; do
+for n in {0..99}; do
     # Replace placeholders in the template script
     sed "s/JOBNAME/$n/g" template_sbatch.sh > temp_job_$n.sh
     sed -i "s/SLURMOBJECT/$n/g" temp_job_$n.sh
