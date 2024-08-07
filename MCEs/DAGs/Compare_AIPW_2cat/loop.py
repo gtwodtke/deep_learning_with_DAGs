7b09a5562f1b8d456cd1bc28704e7d779c497ac1
Jesse Zhou <69114052+JesseZhou-1@users.noreply.github.com>
2024-08-06 22:27:10 -0500

    Update loop.py

--- MCEs/DAGs/Compare_AIPW_2cat/loop.py
+++ MCEs/DAGs/Compare_AIPW_2cat/loop.py
@@ -14,7 +14,7 @@ if __name__ == "__main__":
     num_cores = min(48, os.cpu_count())  # Use at most 10 cores, but not more than available
     print(num_cores)
     results = Parallel(n_jobs=num_cores)(delayed(run_simulation)(i, node_id) for i in range(n_iterations))
-    results_df = pd.DataFrame(results, columns=['E_Y_A_0_sim', 'E_Y_A_1_sim', 'ATEhat', 'Time_taken'])
+    results_df = pd.DataFrame(results, columns=['E_Y_A_0_sim', 'E_Y_A_1_sim', 'E_Y_A_0_boot', 'E_Y_A_1_boot', 'ATEhat', 'Time_taken'])
 
     base_path = '/project/wodtke/cGNF_python_code'  # Adjust to your actual path
     folder = '64k'  # Adjust to your actual folder
